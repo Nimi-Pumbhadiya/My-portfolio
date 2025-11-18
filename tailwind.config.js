dbfe2799ec73d776ebd8b629a1b9a701d28da38f
Nimisha Pumbhadiya <158990137+Nimi-Pumbhadiya@users.noreply.github.com>
2025-11-18 19:24:26 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html", "./**/*.js", "!./node_modules/**/*", "!./dist/**/*"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#e6b15f",
+        secondary: "#131818",
+        // accent:'#18312e',
+        accent: "#22021d",
+      },
+    },
+  },
+  corePlugins: {
+    direction: true,
+  },
+};
